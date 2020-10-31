959dfe22124866f1c48d824c46677f4e8e3bf38f
syu-y <syu12580526@gmail.com>
2020-10-31 13:15:48 +0900

    add index.ts

--- chapter2/src/index.ts
+++ chapter2/src/index.ts
@@ -1,1 +1,11 @@
 console.log('Hello TypeScript!!');
+
+// Exercise 2.5
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+  apple: a,
+  banana: b,
+};
+// let d = c.apple + "4"; // これはOK d:string になる
+let d:number = c.apple * 4;
